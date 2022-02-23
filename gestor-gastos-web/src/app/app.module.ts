470998f11fa99eed726d74969227812319b6da74
Jesus <jesusmarquezdelgado@gmail.com>
2022-02-23 19:49:54 +0100

    restored

---
+++ gestor-gastos-web/src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ProjectExpensesComponent } from './project-expenses/project-expenses.component';
+// import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    UserListComponent,
+    UserProfileComponent,
+    ProjectExpensesComponent,
+    // UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDividerModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+    RouterModule.forRoot([
+      { path: 'users', component: UserListComponent },
+      { path: 'users/:username', component: UserProfileComponent },
+      { path: 'projects/:projectId', component: ProjectExpensesComponent },
+      { path: 'test', component: TestComponent },
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
---
+++ gestor-gastos-web/src/app/project-expenses/project-expenses.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Expense } from '../expense';
+
+@Component({
+  selector: 'app-project-expenses',
+  templateUrl: './project-expenses.component.html',
+  styleUrls: ['./project-expenses.component.css']
+})
+export class ProjectExpensesComponent implements OnInit {
+  expenses: any = []
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    const routeParams = this.route.snapshot.paramMap;
+    const projectId = Number(routeParams.get('projectId'));
+    this.http.get('http://127.0.0.1:8000/get_project_expenses/' + projectId)
+      .subscribe((res) => {
+      this.expenses = Expense.jsontoList(res);
+      console.log(res)
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
