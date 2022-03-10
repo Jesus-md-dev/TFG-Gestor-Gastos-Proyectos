9cad7797630d4945a0a933824a042dea1a18d32e
Jesús <jesusmarquezdelgado@gmail.com>
2022-03-10 19:09:11 +0100

    user without api

--- gestor-gastos-web/src/app/user-list/user-list.component.ts
+++ gestor-gastos-web/src/app/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import UsersList from '../userlist.json';
 
 @Component({
   selector: 'app-user-list',
@@ -9,12 +10,28 @@ import { User } from '../user';
 })
 export class UserListComponent implements OnInit {
   users: any = []
-
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('http:///127.0.0.1:8000/get_all_users/').subscribe((res) => {
+    try {
+      this.http.get('http:///127.0.0.1:8000/get_all_users/').subscribe((res) => {
       this.users = User.jsontoList(res);
-    })
+      console.log(res)
+
+    });
+    } catch (error) {
+
+    }
+
+    if (this.users.length === 0) {
+      this.users = User.jsontoList(UsersList);
+    }
   }
+}
+
+function saveText(text: string | number | boolean, filename: string){
+  var a = document.createElement('a');
+  a.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(text));
+  a.setAttribute('download', filename);
+  a.click()
 }
