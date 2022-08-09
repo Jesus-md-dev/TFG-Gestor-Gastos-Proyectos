ae87c43891295224d69a6379d7263368e8a81bc0
Jesus <jesusmarquezdelgado@gmail.com>
2022-08-09 21:52:53 +0200

    Change App name and icon

--- project-expenses-manager/karma.conf.js
+++ project-expenses-manager/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gestor-gastos-web'),
+      dir: require('path').join(__dirname, './coverage/project-expenses-manager'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- project-expenses-manager/src/app/app.component.spec.ts
+++ project-expenses-manager/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gestor-gastos-web'`, () => {
+  it(`should have as title 'project-expenses-manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gestor-gastos-web');
+    expect(app.title).toEqual('project-expenses-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gestor-gastos-web app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('project-expenses-manager app is running!');
   });
 });
