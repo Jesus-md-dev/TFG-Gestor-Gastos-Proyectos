36512d3feba9388ef6d7dc514165afbfca441a66
Jesus <jesusmarquezdelgado@gmail.com>
2022-08-22 14:20:56 +0200

    css changes

--- project-expenses-manager/src/app/global-component.ts
+++ project-expenses-manager/src/app/global-component.ts
@@ -1,5 +1,5 @@
 export class GlobalComponent {
-  public static apiUrl: string = 'http://13.37.212.41';
+  public static apiUrl: string = 'http://127.0.0.1:8000/';
   public static companyEmail: string = 'PemService@outlook.com';
   public static blankUserImgPath = '/assets/img/blankprofileimg.jpg';
   public static blankProjectImgPath = '/assets/img/blankprojectimg.jpg';
